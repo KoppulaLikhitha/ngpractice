4474f58d935b09f5aff5ada7f714f44cde4be1f3
KoppulaLikhitha <likhithakoppula912@gmail.com>
2021-08-30 19:54:44 +0530

    myfirst ng project

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/dist/myfirstngproj'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
